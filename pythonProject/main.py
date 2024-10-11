05a564df9950253985793ee2e84a409957dc5eda
marcoHosseini <hosseinimorteza@outlook.de>
2024-10-11 16:00:01 +0200

    feat cipher

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,8 +1,17 @@
-import multiprocessing
+def caesarcipher(text_to_encrypt, shift_value):
+    encrypted_text = ""
+    for char in text_to_encrypt:
+        if char.isalpha():
+            ascii_offset = 65 if char.isupper() else 97
+            new_codepoint = (ord(char) - ascii_offset + shift_value) % 26 + ascii_offset
+            new_alphabet = chr(new_codepoint)
+            encrypted_text += new_alphabet
+        else:
+            encrypted_text += char
+    return encrypted_text
 
-from chapter1 import kreis, calc, bookCalc
 
+text_to_encrypt_user_input = input("please give your text to encrypt: ")
+shift_value_user_input = int(input("please give your shift value: "))
 
-bookCalc(1096,16)
-
-print(multiprocessing.cpu_count())
+print(caesarcipher(text_to_encrypt_user_input, shift_value_user_input))
---
+++ pythonProject/testpackage/testpackage.py
@@ -0,0 +1,1 @@
+print("gratulate this is the first understanding packages")
